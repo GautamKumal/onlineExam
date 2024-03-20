e53f426a1846633a8f1ebb60fb142bb759ed9072
Gautam <MR.G @gmail.com>
2024-03-20 13:45:41 +0545

    Nthg

---
+++ oems/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Register from "@/components/dashBoard/register/Register";
+
+export default function Page() {
+  return (
+    <>
+      <Register />;
+    </>
+  );
+}
---
+++ oems/src/components/dashBoard/register/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+      Register hello
+    </div>
+  )
+}
+
+export default Register
